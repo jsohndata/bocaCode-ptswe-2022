85bd736e05b9ca8f0adb02d176ef9b71e252ce65
Jiho Sohn <jihos@me.com>
2022-06-01 20:06:22 -0400

    Array index insight

---
+++ w02-d02-array-index.js
@@ -0,0 +1,32 @@
+
+// OBJ
+
+const object = {
+    name1: 1000,
+    name2: 2000,
+    name3: 3000,
+}
+
+let renderResult = object['name1'];
+
+console.log(renderResult);
+
+// Setup: Array
+const arrayMain1 = [
+    "test1",                // [0]
+    "test2",                // [1]
+    "test3",                // [2]
+    "Elizabeth",            // [3]
+    [                       // [4]                       
+        "childarray1",      // [4][0]
+        "childarray2",      // [4][1]
+        "childarray3",      // [4][2]
+        {                   // [4][3]
+            gcname1: "grandchild1",  // [4][3][0]
+            gcname2: "grandchild2",  // [4][3][1]
+            gcname3: "grandchild3",  // [4][3][2]
+        }
+    ]
+]
+
+console.log(arrayMain1[4][3])
